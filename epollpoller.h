7a7259be16f82c3cf1f3a04e0389c936d7ae201d
Sim-szm <xianszm007@163.com>
2014-04-21 20:00:51 +0800

    epollpoller

---
+++ epollpoller.h
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  epollpoller.h
+ *        Version:  1.0
+ *        Created:  2014年04月20日 18时37分46秒
+ *       Revision:  none
+ *       Compiler:  clang
+ *         Author:  sim szm, xianszm007@gmail.com
+ *        Company:  Class 1107 of Computer Science and Technology
+ *
+ * =====================================================================================
+ */
+#ifndef EPOLLPOLLER_H
+#define EPOLLPOLLER_H
+
+#include <assert>
+#include <sys/epoll.h>
+#include <map>
+#include <vector>
+
+//...wait
+
+
+#endif 
